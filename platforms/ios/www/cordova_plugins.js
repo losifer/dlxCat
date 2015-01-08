09864a881caa7ec9127b857fa51ad6f490fd202e
Carlos Canas <carloscanash@gmail.com>
2015-01-07 16:14:38 -0800

    DLXSF-Spring15-Cordova
    Cordova with Xcode files - V 1.0.2

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.statusbar": "0.1.9",
+    "org.apache.cordova.splashscreen": "0.3.5"
+}
+// BOTTOM OF METADATA
+});
